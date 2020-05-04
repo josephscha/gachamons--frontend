358a819155b24e58d39981749f029dee63e98a2d
Yueying <wangyy395@gmail.com>
2020-05-04 14:59:38 -0400

    add home page

--- src/script.js
+++ src/script.js
@@ -2,20 +2,22 @@ const monstersUrl = 'http://127.0.0.1:3000/monsters'
 
 document.addEventListener('DOMContentLoaded', function(){
 
-    function getMonsters() {
-        fetch(monstersUrl)
-        .then(res => res.json())
-        .then(function(result) {
-            debugger;
-            result.forEach(function(monster){
-                showMonster(monster)
-            })
-        })
-    }
+    // function getMonsters() {
+    //     fetch(monstersUrl)
+    //     .then(res => res.json())
+    //     .then(function(result) {
+    //         result.forEach(function(monster){
+    //             showMonster(monster)
+    //         })
+    //     })
+    // }
 
-    function showMonster(monster) {
+    // function showMonster(monster) {
 
-    }
+    //     let img = document.createElement('img')
+    //     img.setAttribute('src', monster.img_url)
+    //     document.body.append(img);
+    // }
 
-    getMonsters();
+    // getMonsters();
 })
